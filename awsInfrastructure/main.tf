1a4dbb784252e0b67881c1204d42c917cc83d2f8
08abhinav <abhinavnegi101@gmail.com>
2026-03-30 20:52:44 +0530

    provisioned aws instance and data block

---
+++ awsInfrastructure/main.tf
@@ -0,0 +1,69 @@
+data "aws_ami" "ubuntu"{
+  most_recent = true
+  owners = ["099720109477"]
+
+  filter {
+    name = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+module "network" {
+    source = "./network"
+}
+
+resource "aws_instance" "control_node"{
+    ami = data.aws_ami.ubuntu.id
+    key_name = var.keyPair
+    instance_type = var.instance_type
+    
+    vpc_security_group_ids = [ module.network.security_group ]
+    subnet_id = module.network.subnet_id
+
+    tags = {
+        Name = "control-node"
+    }
+}
+
+resource "aws_instance" "managed_node1"{
+    ami = data.aws_ami.ubuntu.id
+    key_name = var.keyPair
+    instance_type = var.instance_type
+    
+    vpc_security_group_ids = [ module.network.security_group ]
+    subnet_id = module.network.subnet_id
+
+    tags = {
+        Name = "managed-node1"
+    }
+}
+
+resource "aws_instance" "managed_node2"{
+    ami = data.aws_ami.ubuntu.id
+    key_name = var.keyPair
+    instance_type = var.instance_type
+    
+    vpc_security_group_ids = [ module.network.security_group ]
+    subnet_id = module.network.subnet_id
+
+    tags = {
+        Name = "managed-node2"
+    }
+}
+
+output "control-node-ip"{
+    value = aws_instance.control_node
+}
+
+output "managed-node1-ip"{
+    value = aws_instance.managed_node1
+}
+
+output "managed-node2-ip"{
+    value = aws_instance.managed_node2
+}
